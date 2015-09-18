9aa9ffc9e819bd3f07de51a48f755c24ae6481e4
Stijn de Witt <StijnDeWitt@hotmail.com>
2015-09-18 20:08:59 +0200

    Added tests for dependencies

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,17 @@
 'use strict';
 var amd = require('./amd');
+var amdWithDeps = require('./amd-with-deps');
 var browser = require('./browser');
+var browserWithDeps = require('./browser-with-deps');
 var cjs = require('./cjs');
 
 tests();
 
 function tests() {
     amd();
+    amdWithDeps();
     browser();
+    browserWithDeps();
     cjs();
 }
 
